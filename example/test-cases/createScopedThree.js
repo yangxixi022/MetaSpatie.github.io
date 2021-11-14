1393fe542667b64e62150004931eb29329c9871e
yangxixi022 <uuuenter@163.com>
2021-11-14 18:12:47 +0800

    update project

---
+++ example/test-cases/createScopedThree.js
@@ -0,0 +1,15 @@
+import {createScopedThreejs} from 'threejs-miniprogram'
+
+Page({
+  onReady() {
+    wx.createSelectorQuery()
+      .select('#webgl')
+      .node()
+      .exec((res) => {
+        const canvas = res[0].node
+        // 创建一个与 canvas 绑定的 three.js
+        const THREE = createScopedThreejs(canvas)
+        // 传递并使用 THREE 变量
+      })
+  }
+})
